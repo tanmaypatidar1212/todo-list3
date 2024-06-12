111d6f55dab1cef9c23263a978d071d8dbe5e478
tanmaypatidar1212 <141327118+tanmaypatidar1212@users.noreply.github.com>
2024-06-12 08:34:13 +0000

    project completed

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// importing all required libraries
+import React from "react";
+import { ReactNotifications } from "react-notifications-component";
+// importing all required components
+import TodoContainer from "./components/container/TodoContainer.js";
+
+// creating an App component
+function App() {
+  return (
+    <div className="App">
+      {/* using ReactNotifications for rendering up all the notifications */}
+      <ReactNotifications />
+      {/* rendering the todo container component */}
+      <TodoContainer />
+    </div>
+  );
+}
+
+// exporting app component by default
+export default App;
